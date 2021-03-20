334c7425c63b547ac4467eb9f7a89115f5f06fd1
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-03-20 11:24:19 -0400

    Created seed for stores

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying all stores..."
+Store.destroy_all if Rails.env.development?
+puts "All stores were destroyed."
+
+puts "Creating dummy user."
+user_dummy = User.create(email: "dummy@user.com", password: "123456")
+puts "#{user_dummy.id} was created."
+
+puts "Creating stores..."
+10.times do
+  new_store = Store.create(
+    name: Faker::Company.name,
+    address: Faker::Address.city,
+    description: Faker::Company.catch_phrase,
+    discount_breakpoints: rand(500..3000)
+    )
+  puts "Store ##{new_store.id} was created."
+end
