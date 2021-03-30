99f22ccb1aec8b40db69d4da3f0474302bfc9006
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-03-30 14:56:11 -0400

    defined relation between product, order and order_items

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   belongs_to :store
   has_one :categorie
   has_many_attached :photos
+  has_many :order_items
   # multisearchable against: [:name, :description]
   pg_search_scope :search_by_name_and_description,
     against: [ :name, :description ],
