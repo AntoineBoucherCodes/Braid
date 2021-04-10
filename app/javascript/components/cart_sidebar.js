3e24df8a1f5a1b7e9dfb7dde796a4aa5de44ea5d
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-04-10 15:31:26 -0400

    toggle class on click function working on placeholder

--- app/javascript/components/cart_sidebar.js
+++ app/javascript/components/cart_sidebar.js
@@ -1,11 +1,9 @@
-const toggleSidebarTest = () => {
-  console.log("hi test");
-};
-
 const initToggleSidebarOnClick = () => {
-  const cart = document.getElementById('cart_sidebar');
+  const cart = document.querySelector('.cart_sidebar');;
   cart.addEventListener("click", (event) => {
     console.log("Cart was clicked.");
+    cart.classList.toggle("show_sidebar");
+    console.log(cart.classList);
   });
 };
 
