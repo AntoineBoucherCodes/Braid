b3dd2d1f43a2ac1db286ccb5010094c6b1a20475
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2021-04-10 16:30:08 -0400

    sidebar cart now shows users' current cart contents

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, :cart
+  before_action :authenticate_user!, :set_cart
   include Pundit
+  include CurrentCart
 
   after_action :verify_authorized, except: [:index, :dashboard], unless: :skip_pundit?
   after_action :verify_policy_scoped, only: [:index, :dashboard], unless: :skip_pundit?
-
-  def cart
-    @test = "testvariable"
-  end
 
   private
 
